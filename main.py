404fe16c47548994e2f72fc50a190c1cf58ca974
JackJMiller <jackjamesmiller.dev@gmail.com>
2023-03-23 14:13:00 +0000

    add generate and render modes to main.py; render water

--- main.py
+++ main.py
@@ -2,18 +2,26 @@ import json, os, sys
 from src.Terrain import Terrain
 from src.Renderer3D import Renderer3D
 
-renderer = Renderer3D()
-renderer.run()
+print(sys.argv)
+if sys.argv[1] == "generate":
+    print("generating " + sys.argv[1])
+    # locate world configuration
+    WORLD_NAME = sys.argv[2]
+    config_file_path = os.path.join("configs", WORLD_NAME, "CONFIG.json")
 
-sys.exit(0)
+    file = open(config_file_path, "r")
+    config = json.load(file)
 
-# locate world configuration
-WORLD_NAME = sys.argv[1]
-config_file_path = os.path.join("configs", WORLD_NAME, "CONFIG.json")
+    # generate terrain according to configuration
+    terrain = Terrain(WORLD_NAME, config)
 
-file = open(config_file_path, "r")
-config = json.load(file)
+elif sys.argv[1] == "render":
+    print("rendering " + sys.argv[1])
+    WORLD_NAME = sys.argv[2]
+    renderer = Renderer3D(WORLD_NAME)
+    renderer.run()
 
-# generate terrain according to configuration
-terrain = Terrain(WORLD_NAME, config)
+else:
+    sys.exit(1)
+
 
--- src/Renderer3D.py
+++ src/Renderer3D.py
@@ -1,4 +1,5 @@
 import json, math, os, simplepbr, sys
+import random
 
 import src.constants as constants
 
@@ -7,10 +8,12 @@ from direct.task import Task
 from panda3d.core import PointLight
 
 class Renderer3D(ShowBase):
-    def __init__(self):
+    def __init__(self, world_name):
         ShowBase.__init__(self)
 
         simplepbr.init()
+
+        self.world_name = world_name
 
         # position of camera and light
         self.camera_x = 0
@@ -24,19 +27,23 @@ class Renderer3D(ShowBase):
 
         # add tasks to the task manager
         self.taskMgr.add(self.move_camera_task, "MoveCameraTask")
-        self.load_chunk("Mundito", 3, 4)
+        self.load_chunk(0, 10)
 
     # load specified chunk
-    def load_chunk(self, world_name, chunk_q, chunk_r):
-        filepath = os.path.join("worlds", world_name, "chunks", str(chunk_q) + "x" + str(chunk_r) + ".json")
+    def load_chunk(self, chunk_q, chunk_r):
+        filepath = os.path.join("worlds", self.world_name, "chunks", str(chunk_q) + "x" + str(chunk_r) + ".json")
         file = open(filepath, "r")
         save_object = json.load(file)
         for x in range(constants.CHUNK_SIZE):
             for y in range(constants.CHUNK_SIZE):
-                model = self.loader.loadModel("models/grass_block.egg")
-                model.setPos(x, y, 0)
                 height = save_object["map"][x][y][1]
-                height = math.ceil(height / 16)
+                if height < 1:
+                    block_name = "water"
+                    height = 1
+                else:
+                    block_name = "grass"
+                model = self.loader.loadModel("models/" + block_name + "_block.egg")
+                model.setPos(x, y, 0)
                 model.setScale(1, 1, height)
                 model.reparentTo(self.render)
                 self.chunk.append(model)
