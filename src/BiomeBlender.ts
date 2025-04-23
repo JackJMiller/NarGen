001e90892ae53ebd75724d50d61cd7e363b6239b
JackJMiller <jackjamesmiller.dev@gmail.com>
2025-04-23 12:31:50 +0100

    update Rainvalley sample world

--- src/BiomeBlender.ts
+++ src/BiomeBlender.ts
@@ -12,21 +12,21 @@ class BiomeBlender {
     public determineBiomeBlend(x: number, y: number, chunk: Chunk): BiomeBlend {
         let height1 = chunk.biomeSuperGrid.valueAt(x, y);
         let height2 = chunk.perlinOverlayed.valueAt(x, y);
-        let biomeBalance = this.determineBiomeByHeight(height1, height2, chunk);
-        return biomeBalance;
+        let biomeBlend = this.determineBiomeByHeight(height1, height2, chunk);
+        return biomeBlend;
     }
 
     public determineBiomeByHeight(height1: number, height2: number, chunk: Chunk): BiomeBlend {
-        let { value: biomeObj, lowerPoint, upperPoint, index } = chunk.biomesRangerray.select(height1);
+        let { value: biomeObj, lowerPoint: biomeLowerPoint, upperPoint: biomeUpperPoint, index } = chunk.biomesRangerray.select(height1);
         chunk.parentWorld.biomeSizes[biomeObj.name] += 1;
         let subBiome = biomeObj.selectValue(height2);
 
-        let portionPoint = deinterpolate(lowerPoint, upperPoint, height1);
+        let portionPoint = deinterpolate(biomeLowerPoint, biomeUpperPoint, height1);
         let blendRegion = 0.05;
-        let { partnerIndex, influence } = this.determineBlendPartner(portionPoint, index, blendRegion);
+        let { partnerIndex, partnerInfluence } = this.determineBlendPartner(portionPoint, index, blendRegion);
 
         if (0 <= partnerIndex && partnerIndex < chunk.biomesRangerray.length()) {
-            return this.createBlendObject(subBiome, partnerIndex, influence, height2, chunk);
+            return this.createBlendObject(subBiome, partnerIndex, partnerInfluence, height2, chunk);
         }
         else {
             return [{ biome: subBiome, influence: 1 }];
@@ -34,42 +34,43 @@ class BiomeBlender {
 
     }
 
-    private createBlendObject(subBiome: SubBiome, partnerIndex: number, influence: number, height2: number, chunk: Chunk): BiomeBlend {
-        let balance = [{ biome: subBiome, influence: 1 - influence }];
+    private createBlendObject(subBiome: SubBiome, partnerIndex: number, partnerInfluence: number, height2: number, chunk: Chunk): BiomeBlend {
         let blendedBiome = chunk.biomesRangerray.selectByIndex(partnerIndex);
         let blendedBiomeObj = blendedBiome["value"];
-        subBiome = blendedBiomeObj.selectValue(height2);
-        balance.push({ biome: subBiome, influence: influence });
-        return balance;
+        let partnerSubBiome = blendedBiomeObj.selectValue(height2);
+        return [
+            { biome: subBiome, influence: 1 - partnerInfluence },
+            { biome: partnerSubBiome, influence: partnerInfluence }
+        ];
     }
 
-    private determineBlendPartner(portionPoint: number, index: number, blendRegion: number): { partnerIndex: number, influence: number } {
+    private determineBlendPartner(portionPoint: number, index: number, blendRegion: number): { partnerIndex: number, partnerInfluence: number } {
         if (portionPoint <= blendRegion) {
             return {
                 partnerIndex: index - 1,
-                influence: (1 - deinterpolate(0, blendRegion, portionPoint)) / 2
+                partnerInfluence: (1 - deinterpolate(0, blendRegion, portionPoint)) / 2
             };
         }
         else if (portionPoint >= 1 - blendRegion) {
             return {
                 partnerIndex: index + 1,
-                influence: deinterpolate(1 - blendRegion, 1, portionPoint) / 2
+                partnerInfluence: deinterpolate(1 - blendRegion, 1, portionPoint) / 2
             };
         }
         else {
-            return { partnerIndex: -1, influence: 0 };
+            return { partnerIndex: -1, partnerInfluence: 0 };
         }
     }
 
     public determineBiomeColour(x: number, y: number, chunk: Chunk): Colour {
-        let biomeBalance = chunk.biomeGrid.valueAt(x, y);
-        let biome = biomeBalance[0].biome;
+        let biomeBlend = chunk.biomeGrid.valueAt(x, y);
+        let biome = biomeBlend[0].biome;
         return biome.parentColour;
     }
 
     public determineSubBiomeColour(x: number, y: number, chunk: Chunk): Colour {
-        let biomeBalance = chunk.biomeGrid.valueAt(x, y);
-        let biome = biomeBalance[0].biome;
+        let biomeBlend = chunk.biomeGrid.valueAt(x, y);
+        let biome = biomeBlend[0].biome;
         return biome.colour;
     }
 
