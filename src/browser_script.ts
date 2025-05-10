82398bf237d7e3d944c066ef0145542851a90286
JackJMiller <jackjamesmiller.dev@gmail.com>
2025-05-10 16:19:57 +0100

    make browser script JavaScript

--- src/browser_script.ts
+++ src/browser_script.ts
@@ -1,29 +1,21 @@
-export class VirtualFileSystem {
-
-    constructor() {
-        // TODO
-    }
-    
-    public writeFileSync(filepath: string, data: string): void {
-        // TODO
-    }
-
-    public existsSync(filepath: string): boolean {
-        // TODO
-        return true;
-    }
-
-    public mkdirSync(filepath: string): void {
-        // TODO
-    }
-
+// TODO
+export function writeFileSync(filepath: string, data: string): void {
+    return;
 }
 
+// TODO
+export function existsSync(filepath: string): boolean {
+    return true;
+}
+
+// TODO
+export function mkdirSync(filepath: string): void {
+    return;
+}
+
+// TODO
 export function loadJSON(filepath: string): any {
-    // TODO
-    // @ts-ignore
     return {};
 }
 
 export const SPRITE_IMAGES = [];
-export const fs = new VirtualFileSystem();
